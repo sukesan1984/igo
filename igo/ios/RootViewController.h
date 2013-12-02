f2a7189d357713d27c607a48b1d7cb1283c3c29d
Kosuke Takami <takami.kosuke@dena.com>
2013-12-02 19:12:10 +0900

    Initial Commit

---
+++ igo/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  igoAppController.h
+//  igo
+//
+//  Created by Kosuke Takami on 2013/12/02.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
